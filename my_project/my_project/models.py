000b2bb3479dd6bfde6a7fe07a3136faf01df01c
Tanya <tnblnva@vk.com>
2025-04-10 20:19:52 +0300

    добавлены миграции

--- my_project/my_project/models.py
+++ my_project/my_project/models.py
@@ -4,5 +4,8 @@ class WordCard(models.Model):
     spanish_word = models.CharField(max_length=100)
     russian_translation = models.CharField(max_length=100)
 
+    class Meta:
+        app_label = 'my_project'
+
     def __str__(self):
         return self.spanish_word

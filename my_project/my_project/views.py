4bd893b3b1930256cd4f1a9a9688a1a48d505241
Tanya <tnblnva@vk.com>
2025-04-10 20:07:37 +0300

    в views добавлена функция для передачи слов в шаблон learn_words

--- my_project/my_project/views.py
+++ my_project/my_project/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
+from .models import WordCard
 
-from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
 
 def learn_words(request):
-    return render(request, 'learn_words.html')
+    word_cards = WordCard.objects.all()
+    return render(request, 'learn_words.html', {'word_cards': word_cards})
 
 def add_words(request):
     return render(request, 'add_words.html')
 
 def statistics(request):
     return render(request, 'statistics.html')
-
